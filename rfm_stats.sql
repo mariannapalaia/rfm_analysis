85bae1d8677f514e2a7df056d9e38bb48946b8df
mariannapalaia <143185811+mariannapalaia@users.noreply.github.com>
2025-06-03 10:57:17 +0300

    Update rfm_stats.sql

--- rfm_stats.sql
+++ rfm_stats.sql
@@ -1,7 +1,7 @@
--- lets create the rfm segments and see some stats
+-- lets see some stats
 
 select
-	segment
+    segment
   , count(user_id) customer_count
   , sum(monetary_score) total_net_revenue
   , cast(sum(monetary_score) / count(user_id) as decimal(10,2)) avg_net_revenue_per_customer
